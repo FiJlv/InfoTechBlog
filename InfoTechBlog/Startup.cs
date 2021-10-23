26691de3c5b86e3cd769405ae00b72466d486c20
FiJlv <vladzzzzxd@gmail.com>
2021-10-23 16:03:25 +0300

    test

--- InfoTechBlog/Startup.cs
+++ InfoTechBlog/Startup.cs
@@ -21,6 +21,11 @@ namespace InfoTechBlog
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseSession();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -36,10 +41,6 @@ namespace InfoTechBlog
                 });
             });
 
-            app.UseDeveloperExceptionPage();
-            app.UseStatusCodePages();
-            app.UseStaticFiles();
-            app.UseSession();
         }
     }
 }
